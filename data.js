e2cfbc123314fd89108cd9474d36dd92df07355f
Jane-bit <melnikjane22@gmail/com>
2021-11-08 01:42:59 +0300

    test

---
+++ data.js
@@ -0,0 +1,543 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "Перекресток",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.04659840580224284,
+          "pitch": 0.04212027732721779,
+          "rotation": 0,
+          "target": "1--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.2565267879007713,
+          "pitch": 0.12777946515177518,
+          "title": "ул. Мичуринская",
+          "text": ""
+        },
+        {
+          "yaw": -2.87065953993031,
+          "pitch": 0.12100628978920724,
+          "title": "ул. Куйбышева",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "1--",
+      "name": "Вход улица",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4637998410528752,
+          "pitch": 0.07305500345234428,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 0.0045511658336874206,
+          "pitch": 0.044926916724580934,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.22343779042510548,
+          "pitch": 0.1032955449614974,
+          "title": "Режим работы",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "Холл",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.709207341412691,
+        "pitch": 0.0009337007693162036,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6907804741826418,
+          "pitch": 0.19249933762831617,
+          "rotation": 0,
+          "target": "10---"
+        },
+        {
+          "yaw": 3.074066385931374,
+          "pitch": 0.12253478784805694,
+          "rotation": 0,
+          "target": "3-wildcolor-1"
+        },
+        {
+          "yaw": 2.6890983091857805,
+          "pitch": 0.25491543218193513,
+          "rotation": 4.71238898038469,
+          "target": "8-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-wildcolor-1",
+      "name": "WILDCOLOR 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -3.047914683854664,
+        "pitch": 0.07959525793055278,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8299756637948335,
+          "pitch": 0.16900641253011983,
+          "rotation": 4.71238898038469,
+          "target": "4-wildcolor-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.4669669832452357,
+          "pitch": 0.12012436128886961,
+          "title": "&nbsp;Только профессиональные средства для Ваших волос",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4-wildcolor-2",
+      "name": "WILDCOLOR 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -1.402253393213016,
+        "pitch": 0.029924278969058804,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5893684102689036,
+          "pitch": 0.25778017211734827,
+          "rotation": 10.995574287564278,
+          "target": "5-wildcolor--3"
+        },
+        {
+          "yaw": -2.3210675033261268,
+          "pitch": 0.1381450125726431,
+          "rotation": 7.0685834705770345,
+          "target": "6-wildcolor-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.09707285011177547,
+          "pitch": 0.07761641471461189,
+          "title": "что-то про ногти",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.4651791191602523,
+          "pitch": -0.318015035349708,
+          "title": "С полным перечнем услуг&nbsp; Вы можете ознакомиться на нашем сайте",
+          "text": "https://www.wildcolor.club/"
+        }
+      ]
+    },
+    {
+      "id": "5-wildcolor--3",
+      "name": "WILDCOLOR  3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 3.0782827568147617,
+        "pitch": 0.09388765109097186,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6052448351399002,
+          "pitch": 0.266476889894701,
+          "rotation": 5.497787143782138,
+          "target": "9-"
+        },
+        {
+          "yaw": 0.9488658563392836,
+          "pitch": 0.5281065434819254,
+          "rotation": 7.0685834705770345,
+          "target": "7-wildcolor-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.076637392950407,
+          "pitch": -0.1141038479253389,
+          "title": "Что-то про реснички",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "6-wildcolor-",
+      "name": "WILDCOLOR выход",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8040422996379295,
+          "pitch": 0.2744442839604826,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 2.8688598801422858,
+          "pitch": 0.1881906352620888,
+          "rotation": 4.71238898038469,
+          "target": "4-wildcolor-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-wildcolor-",
+      "name": "WILDCOLOR ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.487390589767532,
+          "pitch": 0.15777984619973573,
+          "rotation": 10.995574287564278,
+          "target": "5-wildcolor--3"
+        },
+        {
+          "yaw": 0.4970428654462058,
+          "pitch": 0.14655676235074466,
+          "rotation": 7.853981633974483,
+          "target": "6-wildcolor-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-",
+      "name": "Уборная",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -3.084401117844642,
+        "pitch": 0.005328589573226239,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3708752520975125,
+          "pitch": 0.26175837920065703,
+          "rotation": 0,
+          "target": "5-wildcolor--3"
+        },
+        {
+          "yaw": 1.240124488115459,
+          "pitch": 0.37729494552464793,
+          "rotation": 12.566370614359176,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-",
+      "name": "Коридор",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.6138216041405666,
+        "pitch": 0.05166406177927918,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9722410985163954,
+          "pitch": 0.3114990188264226,
+          "rotation": 7.853981633974483,
+          "target": "8-"
+        },
+        {
+          "yaw": -3.0165926423085008,
+          "pitch": 0.3896258918348341,
+          "rotation": 0.7853981633974483,
+          "target": "4-wildcolor-2"
+        },
+        {
+          "yaw": -0.965517348561864,
+          "pitch": 0.18599386874491408,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.604242826200961,
+          "pitch": 0.11581018520430497,
+          "title": "Ресницы",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "10---",
+      "name": "Выход на улицу",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -1.096023699812962,
+        "pitch": -0.015518663750613726,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.0024099284665553,
+          "pitch": 0.0517712917446147,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
